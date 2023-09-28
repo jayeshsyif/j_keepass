cfd5245174ccb77cd8ae011f4a8c9e950b1ae8df
jganatra <jganatra@opentext.com>
2023-09-28 10:45:40 +0530

    Version update and release

--- app/src/main/java/org/j_keepass/AlarmBroadcastReceiver.java
+++ app/src/main/java/org/j_keepass/AlarmBroadcastReceiver.java
@@ -18,68 +18,72 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("@@ JKeePass","recieved alaram");
+        log("@@ JKeePass","received alarm");
         showNotification(context);
-        Log.i("@@ JKeePass","recieved alaram done");
+        log("@@ JKeePass","received alarm done");
     }
 
     void showNotification(Context context) {
         try {
-            Log.i("@@ JKeePass","recieved alaram 1");
+            log("@@ JKeePass","received alarm 1");
             String CHANNEL_ID = "" + context.getString(R.string.app_name) + "-channel";// The id of the channel.
             CharSequence name = context.getResources().getString(R.string.app_name)+" - Reminder";// The user-visible name of the channel.
             NotificationCompat.Builder mBuilder;
-            Log.i("@@ JKeePass","recieved alaram 2");
+            log("@@ JKeePass","received alarm 2");
             Intent notificationIntent = new Intent(context, LoadActivity.class);
             Bundle bundle = new Bundle();
             notificationIntent.putExtras(bundle);
-            Log.i("@@ JKeePass","recieved alaram 3");
+            log("@@ JKeePass","received alarm 3");
             notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-            Log.i("@@ JKeePass","recieved alaram 4");
+            log("@@ JKeePass","received alarm 4");
             PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
-            Log.i("@@ JKeePass","recieved alaram 5");
+            log("@@ JKeePass","received alarm 5");
             NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            Log.i("@@ JKeePass","recieved alaram 6");
+            log("@@ JKeePass","received alarm 6");
             if (Build.VERSION.SDK_INT >= 26) {
-                Log.i("@@ JKeePass","recieved alaram 7");
+                log("@@ JKeePass","received alarm 7");
                 NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_HIGH);
                 mNotificationManager.createNotificationChannel(mChannel);
-                Log.i("@@ JKeePass","recieved alaram 8");
+                log("@@ JKeePass","received alarm 8");
                 mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLights(Color.RED, 300, 300)
                         .setColor(context.getColor(R.color.kp_changing_white))
                         .setChannelId(CHANNEL_ID)
                         .setContentTitle(name);
-                Log.i("@@ JKeePass","recieved alaram 9");
+                log("@@ JKeePass","received alarm 9");
             } else {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    Log.i("@@ JKeePass","recieved alaram 10");
+                    log("@@ JKeePass","received alarm 10");
                     mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
                             .setSmallIcon(R.mipmap.ic_launcher)
                             .setPriority(Notification.PRIORITY_HIGH)
                             .setColor(context.getColor(R.color.kp_changing_white))
                             .setContentTitle(name);
-                    Log.i("@@ JKeePass","recieved alaram 11");
+                    log("@@ JKeePass","received alarm 11");
                 } else {
-                    Log.i("@@ JKeePass","recieved alaram 12");
+                    log("@@ JKeePass","received alarm 12");
                     mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
                             .setSmallIcon(R.mipmap.ic_launcher)
                             .setPriority(Notification.PRIORITY_HIGH)
                             .setContentTitle(name);
-                    Log.i("@@ JKeePass","recieved alaram 13");
+                    log("@@ JKeePass","received alarm 13");
                 }
             }
-            Log.i("@@ JKeePass","recieved alaram 14");
+            log("@@ JKeePass","received alarm 14");
             mBuilder.setContentIntent(contentIntent);
-            Log.i("@@ JKeePass","recieved alaram 15");
-            mBuilder.setContentText("Keep your passwords update with JKeePass app.");
+            log("@@ JKeePass","received alarm 15");
+            mBuilder.setContentText("Keep your passwords updated with JKeePass app.");
             mBuilder.setAutoCancel(true);
-            Log.i("@@ JKeePass","recieved alaram 16");
+            log("@@ JKeePass","received alarm 16");
             mNotificationManager.notify(1, mBuilder.build());
-            Log.i("@@ JKeePass","recieved alaram 17");
+            log("@@ JKeePass","received alarm 17");
         } catch (Exception e) {
-            Log.i("@@ JKeePass","recieved alaram, exception is "+e.getMessage());
+            log("@@ JKeePass","received alarm, exception is "+e.getMessage());
         }
+    }
+    private void log(String tag, String msg)
+    {
+        //Log.i(tag, msg);
     }
 }
